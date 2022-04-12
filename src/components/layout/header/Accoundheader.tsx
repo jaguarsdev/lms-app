e2e85ed4e05a5ce549a4491c29e72ebf8a3bf0ca
jaguars.dev <jaguarsdev.com@gmail.com>
2022-04-12 23:47:15 +0430

    add darklight

--- src/components/layout/header/Accoundheader.tsx
+++ src/components/layout/header/Accoundheader.tsx
@@ -1,36 +1,12 @@
 import React from 'react';
 import profile_img from '../assets/images/profile.jpg'
+import Darkandlight from './Darkandlight';
 
 
-// document.querySelector('.dayNight input').addEventListener('click', () => {
-//     if( document.getElementById('htmlb').classList.value.indexOf('dark') < 0 ){
-//         document.getElementById('htmlb').classList.add('dark')
-//     } else {
-//         document.getElementById('htmlb').classList.remove('dark')
-//     }
-// })
-
-const clickHandler = () =>{
-    if( document.getElementById('htmlb').classList.value.indexOf('dark') < 0 ){
-        document.getElementById('htmlb').classList.add('dark')
-    } else {
-        document.getElementById('htmlb').classList.remove('dark')
-    }
-    window.document.
-}
 const Accoundheader = () => {
     return (
         <div id="account" className="flex gap-3 items-center">
-            <div>
-                <label className="dayNight">
-                    <input type="checkbox" onClick={clickHandler} />
-                    <div></div>
-                </label>
-                
-                {/* <a className="dribbble" href="https://dribbble.com/shots/5332195-Day-Night-Switch" target="_blank">
-                <img src="https://cdn.dribbble.com/assets/dribbble-ball-mark-2bd45f09c2fb58dbbfb44766d5d1d07c5a12972d602ef8b32204d28fa3dda554.svg" alt="" />
-                </a> */}
-            </div>
+            <Darkandlight />
             <div id="login" className="md:hidden">
                 <div id="avatar" className="flex items-center">
                     <img src={profile_img} className="w-10 h-10 rounded-full mr-3 ring-offset-2 ring-2 ring-offset-black ring-white" alt="" />
---
+++ src/components/layout/header/Darkandlight.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+const Darkandlight = () => {
+    return (
+        <div>
+            <label className="dayNight">
+                <input type="checkbox" />
+                {/* onClick={clickHandler}  */}
+                <div></div>
+            </label>
+        </div>
+    );
+}
+
+export default Darkandlight;
